e3778170b87e155144c1ac199436c2074606ea62
Gabe <finkegabriel@gmail.com>
2020-02-07 19:20:58 -0700

    first commit

---
+++ src/main/java/components/Ballcollector.java
@@ -0,0 +1,13 @@
+
+public class Ballcollector {
+    Ballcollector myBallCollector = new Ballcollector();
+
+    public Ballcollector(){
+
+    }
+
+    public double belt_speed(double speed,boolean inverted){
+        
+        return speed;
+    }
+}
